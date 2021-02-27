c9b02288f15fe9ec346a2ae015014c885cdbf317
Toma Morisawa <morisawa@mizuuchi.lab.tuat.ac.jp>
2021-02-27 16:32:24 +0900

    update

--- sound/sound.ino
+++ sound/sound.ino
@@ -1,12 +1,13 @@
 #include <Adafruit_NeoPixel.h>
  
 #define PIN 6       //信号端子のピン番号
-#define LED_NUM 20  //LEDの数
-#define DELAY 50  // 時間幅
- 
+#define LED_NUM 10  //LEDの数
+#define DELAY 5  // 時間幅
+#define WINDOW_SIZE 20
+
 Adafruit_NeoPixel ledtape = Adafruit_NeoPixel(LED_NUM, PIN, NEO_GRB + NEO_KHZ800);  //3つめのパラメータは使うLEDテープの制御ICがWS2812ならNEO_KHZ800 / WS2811ならNEO_KHZ400
 
-int windows[] = {0, 0, 0, 0, 0};
+int windows[WINDOW_SIZE] = {};
 
 void setup()
 {
@@ -15,36 +16,38 @@ void setup()
   ledtape.show();   //一旦全てOFFの状態で反映
 }
 
-int smooth(int* win, int val) {
-  for (int i = 1; i < sizeof(win) - 1; i++) {
+int smooth(int win[], int len, int val) { 
+  for (int i = len-1; i > 0; i--) {
     win[i] = win[i-1];
   }
   win[0] = val;
 
   int sum = 0;
-  for (int i = 0; i < sizeof(win) - 1; i++) {
+  for (int i = 0; i < len - 1; i++) {
     sum += win[i];
   }
-  return sum / sizeof(win);
+  Serial.println(sum,DEC);//print the sound value to serial
+  return sum / len;
 }
 
 void loop()
 {
   int val;
   uint16_t i, j;
-  
+
   val=analogRead(0);   //connect mic sensor to Analog 0
   //Serial.println(val,DEC);//print the sound value to serial
 
-  int val2 = smooth(windows, val);
+  int val2 = smooth(windows, WINDOW_SIZE, val);
+  //int val2 = val;
   
   if(val2 >= 255){
     val2 = 255;
   }
-  Serial.println(val2,DEC);//print the sound value to serial
+  //Serial.println(val2,DEC);//print the sound value to serial
 
   for(i=0; i < ledtape.numPixels(); i++) {
-    ledtape.setPixelColor(i, val, val, val);
+    ledtape.setPixelColor(i, val2, val2, val2);
   }
 
 /*
